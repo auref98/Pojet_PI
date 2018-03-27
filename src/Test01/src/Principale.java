0d286a266229d749f3521a5de58addba4f69ea15
WereWolfZX <37832137+WereWolfZX@users.noreply.github.com>
2018-03-27 12:46:20 +0200

    test

---
+++ src/Test01/src/Principale.java
@@ -0,0 +1,55 @@
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.event.ContainerAdapter;
+import java.awt.event.ContainerEvent;
+
+public class Principale extends JFrame
+{
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					Principale frame = new Principale();
+					frame.setVisible(true);
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Principale()
+	{
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		int heightScreen = (int)dimension.getHeight();
+		int widthScreen  = (int)dimension.getWidth();
+		
+		int width = (int)(widthScreen * 0.35);
+		int height = (int) (heightScreen * 0.95);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Placer Bateaux");
+		this.setSize(500, 350);
+		this.setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+	}
+
+}
