90ff6f07430181913a24f5a3526f5262c498b652
davemo05 <37833613+davemo05@users.noreply.github.com>
2018-03-28 11:19:08 +0200

    28/03/18-11:18 Constructeur

--- src/Projet_PI/src/Class_PI/Inscription.java
+++ src/Projet_PI/src/Class_PI/Inscription.java
@@ -2,11 +2,16 @@ package Class_PI;
 
 class Inscription {
   private boolean valide;
+  private Representant representant;
+  private Plage plage;
 
-  private Representant ajouterRepresentant;
-  private Plage ajouterPlage;
-
-  Inscription(){
-
+  //constructeur 
+  //pré:le representant est la personne qui lance l'inscription, la plage est celle a laquelle il veut participer
+  //post: les attibuts sont initialisés
+  Inscription(Representant r, Plage p){
+	  	valide = false; //en attendant que le charge de comm ne valide la présence l'attribut est a false
+	  	representant = r;
+	  	plage = p;
   }
+  
 }
