782f3f29ee996645f1f9407c390de20ecf804fde
Daleky45 <robmatpoc@gmail.com>
2018-03-28 11:25:47 +0200

    28/03/18-11:25
    ajout constructeur plage

--- src/Projet_PI/src/Class_PI/Plage.java
+++ src/Projet_PI/src/Class_PI/Plage.java
@@ -6,13 +6,18 @@ import java.util.LinkedList;
 
 class Plage {
   private LocalDate date;
-  private LocalTime heurDebut;
-  private LocalTime heurFin;
+  private LocalTime heureDebut;
+  private LocalTime heureFin;
 
   private Evenement eve;
-  private LinkedList<Inscription> inscri;
+  private LinkedList<Inscription> listeInscri;
 
-  Plage(){
-
+  //constructeur de plage
+  //pré:variables correctement initialisées
+  //post:variables correctement ajoutées
+  Plage(LocalDate date,LocalTime heureDebut,LocalTime heureFin){
+	  this.date=date;
+	  this.heureDebut=heureDebut;
+	  this.heureFin=heureFin;
   }
 }
