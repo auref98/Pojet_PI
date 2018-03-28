7491d9db0459ae0b4e6ee99076d3599d27cc2944
Daleky45 <robmatpoc@gmail.com>
2018-03-28 10:56:17 +0200

    28/03/18-10:55
    ajout du constructeur commentaire

--- src/Projet_PI/src/Class_PI/Commentaire.java
+++ src/Projet_PI/src/Class_PI/Commentaire.java
@@ -4,8 +4,17 @@ class Commentaire {
   private String contenu;
 
   private Representant poste;
-
+  
+  //constructeur commentaire vide
+  //pré:
+  //post:contenu initialisé a null
   Commentaire(){
-
+	  this.contenu=null;
+  }
+  //constructeur commentaire
+  //pré:contenu initialisé, contient au moins un caractère
+  //post:contenu initialisé avec le nouveau contenu
+  Commentaire(String contenu){
+	  this.contenu=contenu;
   }
 }
