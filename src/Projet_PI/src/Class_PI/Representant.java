07b3e6f6195bb9895bbf9f8d731782602fd73de3
Aurelien <afockedey@student.hers.be>
2018-03-28 11:17:56 +0200

    28/03/2018-11.17_Representant
    Constructeur du representant

--- src/Projet_PI/src/Class_PI/Representant.java
+++ src/Projet_PI/src/Class_PI/Representant.java
@@ -2,17 +2,28 @@ package Class_PI;
 
 import java.util.LinkedList;
 
-class Representant {
-  private String nom;
-  private String prenom;
-  private int numTel;
-  private String mail;
-  private String matricule;//matricule -> e16****
+abstract class Representant {
+	private String nom;
+	private String prenom;
+	private int numTel;
+	private String mail;
+	private String matricule;//matricule -> e16****
 
-  private LinkedList<Commentaire> poste;
-  private LinkedList<Inscription> inscri;
-
-  Representant(){
-
-  }
+	private LinkedList<Commentaire> poste;
+	private LinkedList<Inscription> inscri;
+  
+	/**
+	 * @param nom
+	 * @param prenom
+	 * @param numTel
+	 * @param mail
+	 * @param matricule
+	 */
+	public Representant(String nom, String prenom, int numTel, String mail, String matricule) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.numTel = numTel;
+		this.mail = mail;
+		this.matricule = matricule;
+	}
 }
