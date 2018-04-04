0bf93e64d41f37ea846d4130ad007398d0a222c2
Aurelien <afockedey@student.hers.be>
2018-04-04 15:14:54 +0200

    04/04/18-15.14.24
    DAO Commentaire

--- src/Projet_PI/src/DAO/DAOCommentaire.java
+++ src/Projet_PI/src/DAO/DAOCommentaire.java
@@ -104,9 +104,23 @@ public class DAOCommentaire extends DAO<Commentaire> {
 		return change;
 	}
 	@Override
-	public boolean delete(Commentaire object) {
-		
-		return false;
+	public boolean delete(Commentaire com) {
+		String query = "DELETE FROM commentaire WHERE id=?";
+		boolean change = false;
+		try{
+			this.prStat = connection.prepareStatement(query);
+			this.prStat.setInt(1, com.getId());
+			change = (this.prStat.executeUpdate()>0)?true:false;
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}finally{
+			try{
+				this.prStat.close();
+			}catch(SQLException e){
+				System.out.println(e.getMessage());
+			}
+		}
+		return change;
 	}
 	
 	
