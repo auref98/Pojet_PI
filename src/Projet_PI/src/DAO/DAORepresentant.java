5a9558787385f02dc443d43e49c8bddb6e5950d6
Aurelien <afockedey@student.hers.be>
2018-04-05 19:17:06 +0200

    05/04/18-19.16
    DAO Representant

--- src/Projet_PI/src/DAO/DAORepresentant.java
+++ src/Projet_PI/src/DAO/DAORepresentant.java
@@ -5,7 +5,10 @@ package DAO;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.LinkedList;
 
+import Bean.Inscription;
+import Bean.Plage;
 import Bean.Representant;
 
 /**
@@ -15,32 +18,87 @@ import Bean.Representant;
 public class DAORepresentant extends DAO<Representant>{
 	PreparedStatement prStat;
 	ResultSet resSet;
+	
 	@Override
 	public Representant find(int id) {
 		// TODO Auto-generated method stub
-		//C'est une class abstact donc on ne peux pas renvoyer d'objet Representant
-		return null;
+		Representant repr = null;
+		String sql = "SELECT * FROM representant WHERE id = ?";
+		try {
+			this.prStat = connection.prepareStatement(sql);
+			this.prStat.setInt(1, id);
+			this.resSet = this.prStat.executeQuery();
+			if(this.resSet.next()){
+				repr = new Representant(id,this.resSet.getString("LASTNAME"),
+										this.resSet.getString("FIRSTNAME"),
+										this.resSet.getInt("PHONE"),
+										this.resSet.getString("MAIL"),
+										this.resSet.getString("MATRICULE"));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}finally{
+			try {
+				this.resSet.close();
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println(e.getMessage());
+			}
+			try {
+				this.prStat.close();
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println(e.getMessage());
+			}
+		}
+		return repr;
+	}
+	
+	public LinkedList<Inscription> findCommentaire(int id){
+		LinkedList<Inscription> ins = new LinkedList<Inscription>();
+		String sql = "SELECT * FROM inscription WHERE refrepr = ?";
+		Representant rep = this.find(id);
+		try {
+			this.prStat = connection.prepareStatement(sql);
+			this.prStat.setInt(1, id);
+			this.resSet = this.prStat.executeQuery();
+			while(this.resSet.next()){
+				Plage p = new Plage();
+				p.setId(this.resSet.getInt("REFPLAGE"));
+				ins.add(new Inscription(this.resSet.getInt("ID"),(this.resSet.getInt("VALIDE") == 1)?true:false,rep,p));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}finally{
+			try {
+				this.resSet.close();
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println(e.getMessage());
+			}
+			try {
+				this.prStat.close();
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println(e.getMessage());
+			}
+		}
+		return ins;
 	}
 
-	/**
-	 * Fonction create du representant
-	 * @param lastName
-	 * @param firstName
-	 * @param phone
-	 * @param mail
-	 * @param matricule
-	 * @return true s'il y a eu des modifications.
-	 */
-	public boolean createRepresentant(String lastName,String firstName,String phone,String mail,String matricule){
+	@Override
+	public boolean create(Representant rep) {
 		boolean change = false;
 		String sql = "INSERT INTO representant(lastName, firstName, phone, mail, matricule) VALUES (?,?,?,?,?)";
 		try {
 			this.prStat = connection.prepareStatement(sql);
-			this.prStat.setString(1, lastName);
-			this.prStat.setString(2, firstName);
-			this.prStat.setString(3, phone);
-			this.prStat.setString(4, mail);
-			this.prStat.setString(5, matricule);
+			this.prStat.setString(1, rep.getLastname());
+			this.prStat.setString(2, rep.getFirstname());
+			this.prStat.setInt(3, rep.getPhone());
+			this.prStat.setString(4, rep.getMail());
+			this.prStat.setString(5, rep.getMatricule());
 			change = (this.prStat.executeUpdate()>0)?true:false;
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -50,28 +108,14 @@ public class DAORepresentant extends DAO<Representant>{
 				this.prStat.close();
 			} catch (Exception e) {
 				// TODO: handle exception
-				System.out.println(e.getMessage());
 			}
 		}
 		return change;
 	}
-	@Override
-	public boolean create(Representant object) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 	
-	/**
-	 * vas faire une UPDATE du representant, tout les parrametre doivent etre initialiser.
-	 * @param id
-	 * @param lastName
-	 * @param firstName
-	 * @param phone
-	 * @param mail
-	 * @param matricule
-	 * @return true si l'update a ete effectuer.
-	 */
-	public boolean updateRepresentant(int id,String lastName,String firstName,String phone,String mail, String matricule){
+	@Override
+	public boolean update(Representant rep) {
+		// TODO Auto-generated method stub
 		boolean change = false;
 		String sql = "UPDATE representant SET lastname = ?,"
 											+ "firstname = ?,"
@@ -80,12 +124,12 @@ public class DAORepresentant extends DAO<Representant>{
 											+ "matricule = ? WHERE id = ?";
 		try {
 			this.prStat = connection.prepareStatement(sql);
-			this.prStat.setString(1, lastName);
-			this.prStat.setString(2, firstName);
-			this.prStat.setString(3, phone);
-			this.prStat.setString(4, mail);
-			this.prStat.setString(5, matricule);
-			this.prStat.setInt(6, id);
+			this.prStat.setString(1, rep.getLastname());
+			this.prStat.setString(2, rep.getFirstname());
+			this.prStat.setInt(3, rep.getPhone());
+			this.prStat.setString(4, rep.getMail());
+			this.prStat.setString(5, rep.getMatricule());
+			this.prStat.setInt(6, rep.getId());
 			change = (this.prStat.executeUpdate()>0)?true:false;
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -100,24 +144,16 @@ public class DAORepresentant extends DAO<Representant>{
 		}
 		return change;
 	}
-	
-	@Override
-	public boolean update(Representant object) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
-	/**
-	 * Delete un representant correspondant a l'id.
-	 * @param id
-	 * @return true s'il y a eu des modification.
-	 */
-	public boolean delete(int id){
+	//? Doit ton supprimer tout les commentaire lié ainsi que toute les inscriptions?
+	@Override
+	public boolean delete(Representant rep) {
+		// TODO Auto-generated method stub
 		boolean change = false;
 		String sql = "DELETE FROM representant WHERE id = ?";
 		try {
 			this.prStat = connection.prepareStatement(sql);
-			this.prStat.setInt(1, id);
+			this.prStat.setInt(1, rep.getId());
 			change = (this.prStat.executeUpdate()>0)?true:false;
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -130,10 +166,5 @@ public class DAORepresentant extends DAO<Representant>{
 			}
 		}
 		return change;
-	}
-	@Override
-	public boolean delete(Representant object) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 }
