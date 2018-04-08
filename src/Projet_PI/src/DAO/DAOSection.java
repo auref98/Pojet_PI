097e5a535cb903b0e1c0fc4645ff5a3379afb3ef
WereWolfZX <37832137+WereWolfZX@users.noreply.github.com>
2018-04-08 15:03:59 +0200

    08/04/18-15:03 fin DAOEvenement

---
+++ src/Projet_PI/src/DAO/DAOSection.java
@@ -0,0 +1,47 @@
+/*
+ * NamingException(Aurélien, Killian, Robin, Louis, Christophe)
+ */
+
+/**
+ * @author ludovic
+ */
+
+package DAO;
+
+import java.sql.*;
+import java.util.*;
+
+import Bean.*;
+
+public class DAOSection extends DAO<Section>
+{
+
+	@Override
+	public Section find(int id)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean create(Section object)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Section object)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Section object)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
