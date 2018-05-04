d98da89a388f35738a670870b6210d5b08e71d33
WereWolfZX <37832137+WereWolfZX@users.noreply.github.com>
2018-05-04 12:27:42 +0200

    -

--- src/Projet_PI/src/Servlet/ServletDemandeMDP.java
+++ src/Projet_PI/src/Servlet/ServletDemandeMDP.java
@@ -8,18 +8,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import Bean.Representant;
+import DAO.DAORepresentant;
 
 @WebServlet("/DemandeMDP")
 public class ServletDemandeMDP extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		String mail = request.getParameter("email");
-		Representant rep;
 		
-		if()
+		if(new DAORepresentant().find(mail) != null) request.setAttribute("inscriptionFailed", true);
+		else request.setAttribute("inscriptionSucess", true);
+		
+		this.getServletContext().getRequestDispatcher("/WEB-INF/Inscription.jsp").forward(request,  response);
 	}
 
 }
