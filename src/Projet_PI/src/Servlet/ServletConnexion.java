0ff5dc7a0786788c48e2d4ab5fb858ef3953f8fd
WereWolfZX <37832137+WereWolfZX@users.noreply.github.com>
2018-05-04 13:51:47 +0200

    -

--- src/Projet_PI/src/Servlet/ServletConnexion.java
+++ src/Projet_PI/src/Servlet/ServletConnexion.java
@@ -29,15 +29,6 @@ import javax.servlet.http.HttpSession;
 import Bean.*;
 import DAO.*;
 
-<<<<<<< HEAD
-/**
- * Classe de type "servlet" pour la page de connexion redirigeant les requêtes adressées à la page principale de l'application, la liste des événements organisés. <br><br> 
- * Redirige vers la page de connexion 
- * @see HttpServlet
- */
-
-=======
->>>>>>> 3f83460c30736d108d7093f65211d90231cd359a
 @WebServlet("/Connexion")	// Balise indiquant au servlet la cible de la requete HTTP déclenchant son appel
 public class ServletConnexion extends HttpServlet
 {
@@ -49,20 +40,6 @@ public class ServletConnexion extends HttpServlet
 	 */
 	public ServletConnexion() {}
 	
-<<<<<<< HEAD
-	/**
-	 * <i>Called by the server (via the service method) to allow a servlet to handle a GET request </i>(summary from <code>HttpServlet</code>). <br><br>
-	 * Méthode redirigeant la requête HTTP vers la page ad hoc.  (En l'occurrence de la page "Connexion" vers elle-même si la connexion a échoué, vers la page "ListeEvenement" si elle a réussi).
-	 * @param request un objet <code>HttpServletRequest</code> contenant la requête que le client a faite au servlet
-	 * @param response un objet <code>HttpServletResponse</code> contenant la réponse que le servlet envoie au client
-	 * @throws IOException si une erreur d'input ou d'output est détectée quand le servlet traite la requête POST
-	 * @throws ServletException si la requête POST n'a pas pu être traitée
-	 * @see <code>HttpServlet#doPost</code>
-	 * @see <code>javax.servlet.GenericServlet#getServletContext()</code>
-	 */
-=======
->>>>>>> 3f83460c30736d108d7093f65211d90231cd359a
-	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		String mail = request.getParameter("email");
@@ -93,10 +70,6 @@ public class ServletConnexion extends HttpServlet
 			if(prof.getFirstName() == null) firstConnection = true;
 			session.setAttribute("professeur", prof);
 		}
-<<<<<<< HEAD
-		// Si les informations de l'utilisateur ont été vérifiées, redirige sur la première page du site
-		if(!failed)this.getServletContext().getRequestDispatcher("/WEB-INF/NewFile.jsp").forward(request,  response);
-=======
 		
 		if(!failed && firstConnection == false)
 		{
@@ -114,6 +87,5 @@ public class ServletConnexion extends HttpServlet
 		{
 			this.getServletContext().getRequestDispatcher("/WEB-INF/Profil.jsp").forward(request,  response);
 		}
->>>>>>> 3f83460c30736d108d7093f65211d90231cd359a
 	}
 }
