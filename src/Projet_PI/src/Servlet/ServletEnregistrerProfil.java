5b8446f07de8f9f8856a8bbb48a68f489660de4f
Aurelien <afockedey@student.hers.be>
2018-05-24 01:10:45 +0200

    24/05/2018-01.10

--- src/Projet_PI/src/Servlet/ServletEnregistrerProfil.java
+++ src/Projet_PI/src/Servlet/ServletEnregistrerProfil.java
@@ -47,81 +47,87 @@ public class ServletEnregistrerProfil extends HttpServlet
 			String mail = request.getParameter("Mail");	
 			String matricule = request.getParameter("Matricule");		
 			String password = request.getParameter("ConfirmerNouveauMotDePasse");
-			
+			String AncienMotDePasse = request.getParameter("AncienMotDePasse");
 			
 			//Etudiant etu = (Etudiant)session.getAttribute("etudiant");
 			//Professeur prof = (Professeur)session.getAttribute("professeur");
 			boolean enregistrementSuccess = false;
 			
-			if(etu != null)
-			{
-				etu.setLastname(lastName);
-				etu.setFirstname(firstName);
-				etu.setPhone(phone);
-				etu.setMail(mail+"@student.hers.be");
-				etu.setMatricule(matricule);
-				etu.setPassword(password);
-				
-				etu.setDateNaissance(LocalDate.parse(request.getParameter("DateNaissance"), DateTimeFormatter.ISO_DATE));			
-				etu.setPaysNaissance(request.getParameter("PaysNaissance"));					
-				etu.setLieuNaissance(request.getParameter("LieuNaissance"));					
-				etu.setNumNational(request.getParameter("NumNational"));			
-				etu.setNationalite(request.getParameter("Nationalite"));				
-				etu.setNumBanque(request.getParameter("NumBanque"));			
-				etu.setSoutienSocial( (request.getParameter("SoutienSocial") != null)?true:false );							
-				etu.setEmplacementEcole(request.getParameter("EmplacementEcole"));		
-				etu.setRole(request.getParameter("Role"));		
-	
-				String localite = request.getParameter("Localite");
-				int codePostal = Integer.parseInt(request.getParameter("CodePostal"));
-				String rue = request.getParameter("Rue");
-				int numero = Integer.parseInt(request.getParameter("Numero"));
-				String boite = request.getParameter("Boite");
-				String pays = request.getParameter("Pays");
-				Adresse adr = new Adresse(-1, localite, codePostal, rue, numero, boite, pays);
-				new DAOAdresse().find(adr);
-				if(adr.getId() == -1) new DAOAdresse().create(adr);
-				etu.setAdr(adr);
-				
-				int idSect = Integer.parseInt(request.getParameter("Section"));
-				Section sec = new DAOSection().find(idSect);
-				etu.setSec(sec);
-				enregistrementSuccess = new DAOEtudiant().update(etu);
-				session.setAttribute("etudiant", etu);
-			}
-			else
-			{
-				prof.setLastname(lastName);
-				prof.setFirstname(firstName);
-				prof.setPhone(phone);
-				prof.setMail(mail+"@hers.be");
-				prof.setMatricule(matricule);
-				prof.setPassword(password);
-				
-				Enumeration sections = request.getParameterNames();
-				DAOProfesseur daoProf = new DAOProfesseur();
-				prof.setEnseigne(daoProf.findEnseigne(prof.getId()));
-				for(Section sect : prof.getEnseigne()){
-					daoProf.removeEnseigne(prof.getId(), sect.getId());
+			Representant repre = etu;
+			if(etu == null)repre = prof;
+			if(repre == null)repre = rep;
+			
+			if(AncienMotDePasse != null && new DAORepresentant().find(repre.getMail(), AncienMotDePasse) != null){
+				if(etu != null)
+				{
+					etu.setLastname(lastName);
+					etu.setFirstname(firstName);
+					etu.setPhone(phone);
+					etu.setMail(mail+"@student.hers.be");
+					etu.setMatricule(matricule);
+					etu.setPassword(password);
+					
+					etu.setDateNaissance(LocalDate.parse(request.getParameter("DateNaissance"), DateTimeFormatter.ISO_DATE));			
+					etu.setPaysNaissance(request.getParameter("PaysNaissance"));					
+					etu.setLieuNaissance(request.getParameter("LieuNaissance"));					
+					etu.setNumNational(request.getParameter("NumNational"));			
+					etu.setNationalite(request.getParameter("Nationalite"));				
+					etu.setNumBanque(request.getParameter("NumBanque"));			
+					etu.setSoutienSocial( (request.getParameter("SoutienSocial") != null)?true:false );							
+					etu.setEmplacementEcole(request.getParameter("EmplacementEcole"));		
+					etu.setRole(request.getParameter("Role"));		
+		
+					String localite = request.getParameter("Localite");
+					int codePostal = Integer.parseInt(request.getParameter("CodePostal"));
+					String rue = request.getParameter("Rue");
+					int numero = Integer.parseInt(request.getParameter("Numero"));
+					String boite = request.getParameter("Boite");
+					String pays = request.getParameter("Pays");
+					Adresse adr = new Adresse(-1, localite, codePostal, rue, numero, boite, pays);
+					new DAOAdresse().find(adr);
+					if(adr.getId() == -1) new DAOAdresse().create(adr);
+					etu.setAdr(adr);
+					
+					int idSect = Integer.parseInt(request.getParameter("Section"));
+					Section sec = new DAOSection().find(idSect);
+					etu.setSec(sec);
+					enregistrementSuccess = new DAOEtudiant().update(etu);
+					session.setAttribute("etudiant", etu);
 				}
-				while(sections.hasMoreElements()){
-					String section = (String)sections.nextElement();
-					String []sect = section.split("-");
-					if(sect[0].equals("section")){
-						int id = -1;
-						try{
-							id = Integer.parseInt(sect[1]);
-						}catch(Exception e){
-							System.out.println("Integer parse -> ServletEnrgistrement");
-						}
-						if(id > 0){
-							daoProf.addEnseigne(prof.getId(), id);
+				else
+				{
+					prof.setLastname(lastName);
+					prof.setFirstname(firstName);
+					prof.setPhone(phone);
+					prof.setMail(mail+"@hers.be");
+					prof.setMatricule(matricule);
+					prof.setPassword(password);
+					
+					Enumeration sections = request.getParameterNames();
+					DAOProfesseur daoProf = new DAOProfesseur();
+					prof.setEnseigne(daoProf.findEnseigne(prof.getId()));
+					for(Section sect : prof.getEnseigne()){
+						daoProf.removeEnseigne(prof.getId(), sect.getId());
+					}
+					while(sections.hasMoreElements()){
+						String section = (String)sections.nextElement();
+						String []sect = section.split("-");
+						if(sect[0].equals("section")){
+							int id = -1;
+							try{
+								id = Integer.parseInt(sect[1]);
+							}catch(Exception e){
+								System.out.println("Integer parse -> ServletEnrgistrement");
+							}
+							if(id > 0){
+								daoProf.addEnseigne(prof.getId(), id);
+							}
 						}
 					}
+					
+					enregistrementSuccess = new DAOProfesseur().update(prof);
+					session.setAttribute("professeur", prof);
 				}
-				
-				enregistrementSuccess = new DAOProfesseur().update(prof);
-				session.setAttribute("professeur", prof);
 			}
 			if(enregistrementSuccess && (boolean)session.getAttribute("firstConnection"))session.setAttribute("firstConnection", false);
 			request.setAttribute("enregistrementSuccess", enregistrementSuccess);
